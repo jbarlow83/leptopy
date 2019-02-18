3a0eab3049b72f79c719bfb3f58f99476fbae1cc
James R. Barlow <jim@purplerock.ca>
2019-02-17 18:29:15 -0800

    Spin off project from ocrmypdf

---
+++ setup.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2013-2019, James R. Barlow
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+if sys.version_info < (3, 6):
+    print("Python 3.6 or newer is required", file=sys.stderr)
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='leptopy',
+    description='Comprehensive bindings for all of Leptonica',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='',
+    license='BSD 2-Clause License',
+    author='James R. Barlow',
+    author_email='jim@purplerock.ca',
+    packages=find_packages('leptonica', exclude=["tests", "tests.*"]),
+    keywords=['PDF', 'OCR', 'optical character recognition', 'PDF/A', 'scanning'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        ],
+    python_requires=' >= 3.6',
+    setup_requires=[
+        'cffi >= 1.9.1',        # to build the leptonica module
+        'setuptools_scm',       # so that version will work
+        'setuptools_scm_git_archive'    # enable version from github tarballs
+    ],
+    use_scm_version={'version_scheme': 'post-release'},
+    cffi_modules=[
+        'leptonica/leptonica_build.py:ffibuilder'
+    ],
+    install_requires=[
+        'cffi >= 1.9.1',          # must be a setup and install requirement
+    ],
+    tests_require=[
+        'pytest >= 3',
+    ],
+    project_urls={
+        'Documentation': '',
+        'Source': '',
+        'Tracker': ''
+    }
+)
